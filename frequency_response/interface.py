c27628ca5542f072ba7a017219706b78b776da4f
jgammell <42384187+jgammell@users.noreply.github.com>
2020-12-06 13:23:45 -0700

    Updated frequency response code

--- frequency_response/interface.py
+++ frequency_response/interface.py
@@ -1,48 +1,183 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 23 14:42:06 2020
-
-@author: jgamm
-"""
-
 #%%
-
-import numpy as np
-from matplotlib import pyplot as plt
-
-center = 100
-amplitude = 50
-settling_periods = 10
-# measurement_periods = 15
-frequency = 1000
 
 import serial
 
+READY_CMD = '1'
+CHECK_AVAILABLE_CMD = '2'
+SET_BASE_CMD = '3'
+SET_FREQ_CMD = '4'
+SET_AMPLITUDE_CMD = '5'
+SET_DURATION_CMD = '6'
+COMPUTE_LUT_CMD = '7'
+BEGIN_CMD = '8'
+SET_KP_CMD = '9'
+SET_KD_CMD = 'A'
+SET_KI_CMD = 'B'
+SET_OFFSET_CMD = 'C'
+ACK = 'a'
+NACK = 'n'
+
+class TRECS:
+    def pitch(self, degrees):
+        #(-0.3656 * analogRead(SENSOR_PIN)) + 185.64
+        val = (degrees-185.64)/(-.3656)
+        return val
+    def flush(self):
+        while self.ser.in_waiting:
+            self.ser.read()
+    def sendCmd(self, cmd):
+        if type(cmd) != bytes:
+            cmd = cmd.encode()
+        s = cmd+b'\n'
+        assert len(s) == len(cmd)+1
+        self.ser.write(s)
+    def readResp(self):
+        resp = self.ser.readline()
+        return resp[:-2].decode()
+    def isAvailable(self):
+        self.sendCmd(CHECK_AVAILABLE_CMD)
+        return self.readResp() == ACK
+    def setBase(self, base):
+        base = int(self.pitch(base))
+        assert base == base&0xFFFF
+        self.sendCmd(SET_BASE_CMD)
+        assert self.readResp() == ACK
+        s = b''.join([((base&0xFF00)>>8).to_bytes(1, 'little'), (base&0xFF).to_bytes(1, 'little')])
+        self.sendCmd(s)
+        assert int(self.readResp()) == base
+        assert self.readResp() == ACK
+    def setFreq(self, freq):
+        freq *= 10
+        freq = int(freq)
+        assert freq == freq&0xFFFF
+        self.sendCmd(SET_FREQ_CMD)
+        assert self.readResp() == ACK
+        s = b''.join([((freq&0xFF00)>>8).to_bytes(1, 'little'), (freq&0xFF).to_bytes(1, 'little')])
+        self.sendCmd(s)
+        assert int(self.readResp()) == freq
+        assert self.readResp() == ACK
+    def setAmp(self, amp):
+        amp = int(self.pitch(amp))
+        assert amp == amp&0xFFFF
+        self.sendCmd(SET_AMPLITUDE_CMD)
+        assert self.readResp() == ACK
+        s = b''.join([((amp&0xFF00)>>8).to_bytes(1, 'little'), (amp&0xFF).to_bytes(1, 'little')])
+        self.sendCmd(s)
+        assert int(self.readResp()) == amp
+        assert self.readResp() == ACK
+    def setDuration(self, periods_measure, periods_wait):
+        assert periods_measure == periods_measure&0xFF
+        assert periods_wait == periods_wait&0xFF
+        self.sendCmd(SET_DURATION_CMD)
+        assert self.readResp() == ACK
+        s = ''.join([chr(periods_measure), chr(periods_wait)])
+        self.sendCmd(s)
+        assert int(self.readResp()) == periods_measure
+        assert int(self.readResp()) == periods_wait
+        assert self.readResp() == ACK
+    def setKp(self, Kp):
+        Kp = int(Kp*1000)
+        assert Kp == Kp&0xFFFF
+        self.sendCmd(SET_KP_CMD)
+        assert self.readResp() == ACK
+        s = b''.join([((Kp&0xFF00)>>8).to_bytes(1, 'little'), (Kp&0xFF).to_bytes(1, 'little')])
+        self.sendCmd(s)
+        assert int(self.readResp()) == Kp
+        assert self.readResp() == ACK
+    def setKd(self, Kd):
+        Kd = int(Kd*1000)
+        assert Kd == Kd&0xFFFF
+        self.sendCmd(SET_KD_CMD)
+        assert self.readResp() == ACK
+        s = b''.join([((Kd&0xFF00)>>8).to_bytes(1, 'little'), (Kd&0xFF).to_bytes(1, 'little')])
+        self.sendCmd(s)
+        assert int(self.readResp()) == Kd
+        assert self.readResp() == ACK
+    def setKi(self, Ki):
+        Ki = int(Ki*1000)
+        assert Ki == Ki&0xFFFF
+        self.sendCmd(SET_KI_CMD)
+        assert self.readResp() == ACK
+        s = b''.join([((Ki&0xFF00)>>8).to_bytes(1, 'little'), (Ki&0xFF).to_bytes(1, 'little')])
+        self.sendCmd(s)
+        assert int(self.readResp()) == Ki
+        assert self.readResp() == ACK
+    def setOffset(self, offset):
+        offset = int(offset)
+        assert offset == offset&0xFFFF
+        self.sendCmd(SET_OFFSET_CMD)
+        assert self.readResp() == ACK
+        s = b''.join([((offset&0xFF00)>>8).to_bytes(1, 'little'), (offset&0xFF).to_bytes(1, 'little')])
+        self.sendCmd(s)
+        assert int(self.readResp()) == offset
+        assert self.readResp() == ACK
+    def computeLut(self):
+        self.sendCmd(COMPUTE_LUT_CMD)
+        assert self.readResp() == ACK
+        values = []
+        while True:
+            resp = self.readResp()
+            if resp == ACK:
+                break
+            values.append(int(resp))
+        return values
+    def begin(self):
+        self.sendCmd(BEGIN_CMD)
+        assert self.readResp() == ACK # Acked command
+        assert self.readResp() == ACK # Finished taking measurements
+        values = []
+        while True:
+            resp = self.readResp()
+            if resp == ACK:
+                break
+            values.append(int(resp))
+        return values
+    def __init__(self, port):
+        self.ser = serial.Serial(port=port, baudrate=115200)
+        resp = self.readResp()
+        if resp != READY_CMD:
+            self.__del__()
+            assert False
+        self.sendCmd(READY_CMD)
+    def __del__(self):
+        self.ser.close()
+
+#%%
+      
+import numpy as np
+from matplotlib import pyplot as plt
+
+offset = 70
+Kp = .05
+Kd = 0
+Ki = 0
+
+base = 0
+frequency = 2 # Hz; Maximum frequency: 72
+amplitude = 1
+periods_measure = 4
+periods_wait = 8
+
 try:
-    TRECS = serial.Serial('COM7', 115200, timeout=1)
-    while TRECS.readline() != b'r\r\n':
-        pass
-    print('TRECS ready')
-    TRECS.write(b'.')
-    while TRECS.readline() != b'A\r\n':
-        pass
-    print('Sending data')
-    TRECS.write(b'%d\n'%(center))
-    TRECS.write(b'%d\n'%(amplitude))
-    TRECS.write(b'%d\n'%(settling_periods))
-    TRECS.write(b'%d\n'%(frequency))
-    print('Done')
-    for i in range(7):
-        print(TRECS.readline().decode())
-    vals = []
-    while True:
-        val = TRECS.readline()
-        val = val.decode()[:-2]
-        if val == 'Done':
-            break
-        else:
-            vals.append(np.int16(val))
-        
+    trecs = TRECS('COM4')
+    assert trecs.isAvailable()
+    trecs.setOffset(offset)
+    trecs.setKp(Kp)
+    trecs.setKd(Kd)
+    trecs.setKi(Ki)
+    trecs.setBase(base)
+    trecs.setAmp(amplitude)
+    trecs.setFreq(frequency)
+    trecs.setDuration(periods_measure, periods_wait)
+    input_values_p = trecs.computeLut()
+    input_values = []
+    for i in range(periods_measure):
+        input_values += input_values_p
+    output_values = trecs.begin()
+    assert len(output_values) == len(input_values)
+    plt.plot(range(len(input_values)), input_values, color='blue')
+    plt.figure()
+    plt.plot(range(len(output_values)), output_values, color='red')
 finally:
-    TRECS.close()
-    plt.plot(range(len(vals)), vals)
+    del trecs
--- frequency_response/measure_response/measure_response.ino
+++ frequency_response/measure_response/measure_response.ino
@@ -1,155 +1,278 @@
 #include "Servo.h"
 #include "MsTimer2.h"
 #include <math.h>
-#include <stdint.h>
-#include <arduino-timer.h>
 
-#define ESC_PIN    (6)
-#define DRIVE_PIN  (10)
+#define ESC_PIN (6)
+#define DRIVE_PIN (10)
 #define SENSOR_PIN (A0)
 
-bool progressState(void *);
+#define READY_CMD ('1')
+#define CHECK_AVAILABLE_CMD ('2')
+#define SET_BASE_CMD ('3')
+#define SET_FREQ_CMD ('4')
+#define SET_AMPLITUDE_CMD ('5')
+#define SET_DURATION_CMD ('6')
+#define COMPUTE_LUT_CMD ('7')
+#define BEGIN_CMD ('8')
+#define SET_KP_CMD ('9')
+#define SET_KD_CMD ('A')
+#define SET_KI_CMD ('B')
+#define SET_OFFSET_CMD ('C')
+#define ERROR_CMD ('e')
+#define ACK   ('a')
+#define NACK  ('n')
 
-#define Nm (5)
-#define NUM_SAMPLES (100)
-int16_t  SIN_LUT[NUM_SAMPLES/4];
-inline int16_t sin_lut(uint16_t x)
-{
-  if(x < (NUM_SAMPLES/4))
-    return SIN_LUT[x];
-  else if(x < (NUM_SAMPLES/2))
-    return SIN_LUT[(NUM_SAMPLES/2) - x - 1];
-  else if(x < (3*NUM_SAMPLES/4))
-    return -1*SIN_LUT[x - (NUM_SAMPLES/2) - 1];
-  else
-    return -1*SIN_LUT[NUM_SAMPLES-x - 1];
-}
-
-typedef enum
-{
-  idle,
-  settling,
-  measuring
-} state_enum;
-
-int16_t center;
+// Experiment parameters
+#define SAMPLES_PER_PERIOD (64)
+int16_t signal[SAMPLES_PER_PERIOD];
+int16_t datapoints[4*SAMPLES_PER_PERIOD];
+int16_t base;
+int16_t frequency;
+uint32_t sample_period_us;
 int16_t amplitude;
-uint8_t Ns;
-uint16_t f;
-state_enum state;
+uint8_t periods_measure;
+uint8_t periods_wait;
+
+// PID parameters
+int16_t target;
+int16_t Kp;
+int16_t Kd;
+int16_t Ki;
+int16_t offset;
 
 Servo ESC;
-Timer<10, micros> timer;
+
+#define setSpeed(DRIVE) ESC.writeMicroseconds((DRIVE)+1000)
+#define Kp(E) ((Kp*(E))/1000)
+#define Kd(E) ((Kd*(E))/1000)
+#define Ki(E) ((Ki*(E))/1000)
+
+void updatePid(void)
+{
+  static int16_t old_error = 0;
+  static int16_t acc_error = 0;
+  int16_t error = target-analogRead(SENSOR_PIN);
+  acc_error += error;
+  int16_t new_drive = Kp(error) + Kd(error-old_error) + Ki(acc_error) + offset;
+  setSpeed(new_drive);
+  old_error = error;
+}
+
+char readCmd(void)
+{
+  while(!Serial.available());
+  char cmd = Serial.read();
+  while(!Serial.available());
+  if(Serial.read() != '\n')
+    cmd = ERROR_CMD;
+  return cmd;
+}
 
 void setup() 
 {
-  center = 0;
-  amplitude = 0;
-  Ns = 0;
-  f = 0;
-  state = idle;
-  
-  // initializations based on original code
+  target = 0;
+  Kp = 0;
+  Kd = 0;
+  Ki = 0;
+  offset = 0;
   Serial.begin(115200);
   ESC.attach(ESC_PIN);
   pinMode(DRIVE_PIN, OUTPUT);
   digitalWrite(DRIVE_PIN, LOW);
-
-  // clear serial buffer
+  delay(500);
+  setSpeed(0);
+  digitalWrite(DRIVE_PIN, HIGH);
+  delay(2500);
+  MsTimer2::stop();
+  MsTimer2::set(20, updatePid);
   while(Serial.available() && Serial.read());
-  Serial.println("r"); // Indicate readiness
+  Serial.println(READY_CMD);
+  while(readCmd() != READY_CMD);
 }
 
-void loop()
+void loop() 
 {
-  if(Serial.available())
+  char cmd = readCmd();
+  switch(cmd)
   {
-    Serial.read();
-    Serial.println("A");
-    while(!Serial.available());
-    center = Serial.parseInt();
-    Serial.read();
-    while(!Serial.available());
-    amplitude = Serial.parseInt();
-    Serial.read();
-    while(!Serial.available());
-    Ns = Serial.parseInt();
-    Serial.read();
-    while(!Serial.available());
-    f = Serial.parseInt();
-    Serial.read();
-      // create lookup table for sin
-    for(uint16_t i = 0; i < NUM_SAMPLES/4; ++i)
-      SIN_LUT[i] = (int16_t) amplitude*sin(i*(2*M_PI/NUM_SAMPLES))+center;
-    Serial.println("Running trial with the following parameters:");
-    Serial.print("\tCenter: "); Serial.print(center); Serial.print('\n');
-    Serial.print("\tAmplitude: "); Serial.print(amplitude); Serial.print('\n');
-    Serial.print("\tSettling periods: "); Serial.print(Ns); Serial.print('\n');
-    Serial.print("\tFrequency: "); Serial.print(f); Serial.print('\n');
-    Serial.print("Sin lookup table:\n");
-    for(uint16_t i=0; i<=NUM_SAMPLES; ++i)
-      Serial.println(sin_lut(i));
-    Serial.println("Done");
-    digitalWrite(DRIVE_PIN, LOW);
-    delay(500);
-    digitalWrite(DRIVE_PIN, HIGH);
-    timer.every(1000000UL/(NUM_SAMPLES*f), progressState);
-  }
-}
-
-uint8_t N;
-uint16_t i;
-uint8_t measurements[NUM_SAMPLES*Nm];
-uint16_t measurements_idx;
-bool progressState(void * arg)
-{
-  (void) arg;
-  switch(state)
-  {
-    case idle:
-      N = Ns;
-      i = 0;
-      state = settling;
+    case CHECK_AVAILABLE_CMD:
+      Serial.println(ACK);
       break;
-    case settling:
-      if(i < NUM_SAMPLES)
+    case SET_BASE_CMD:
+      Serial.println(ACK);
+      while(!Serial.available());
+      base = ((int16_t) Serial.read()) << 8;
+      while(!Serial.available());
+      base |= (int16_t) Serial.read();
+      while(!Serial.available());
+      if(Serial.read() != '\n')
       {
-        ESC.writeMicroseconds(map(sin_lut(i), 0, 1000, 1000, 2000));
-        i += 1;
+        Serial.println("Invalid base command");
+        break;
       }
-      else
+      Serial.println(base);
+      Serial.println(ACK);
+      break;
+    case SET_FREQ_CMD:
+      Serial.println(ACK);
+      while(!Serial.available());
+      frequency = ((int16_t)(uint8_t) Serial.read()) << 8;
+      while(!Serial.available());
+      frequency |= (int16_t)(uint8_t) Serial.read();
+      while(!Serial.available());
+      if(Serial.read() != '\n')
       {
-        N -= 1;
-        if(N == 0)
+        Serial.println("Invalid frequency command");
+        break;
+      }
+      Serial.println(frequency);
+      sample_period_us = 10*1000000UL/(frequency*SAMPLES_PER_PERIOD);
+      Serial.println(ACK);
+      break;
+    case SET_AMPLITUDE_CMD:
+      Serial.println(ACK);
+      while(!Serial.available());
+      amplitude = ((int16_t) Serial.read()) << 8;
+      while(!Serial.available());
+      amplitude |= (int16_t) Serial.read();
+      while(!Serial.available());
+      if(Serial.read() != '\n')
+      {
+        Serial.println("Invalid amplitude command");
+        break;
+      }
+      Serial.println(amplitude);
+      Serial.println(ACK);
+      break;
+    case SET_DURATION_CMD:
+      Serial.println(ACK);
+      while(!Serial.available());
+      periods_measure = (uint8_t) Serial.read();
+      while(!Serial.available());
+      periods_wait = (uint8_t) Serial.read();
+      while(!Serial.available());
+      if(Serial.read() != '\n')
+      {
+        Serial.println("Invalid duration command");
+        break;
+      }
+      Serial.println(periods_measure);
+      Serial.println(periods_wait);
+      Serial.println(ACK);
+      break;
+    case SET_KP_CMD:
+      Serial.println(ACK);
+      while(!Serial.available());
+      Kp = ((int16_t) Serial.read())<<8;
+      while(!Serial.available());
+      Kp |= ((int16_t) Serial.read());
+      while(!Serial.available());
+      if(Serial.read() != '\n')
+      {
+        Serial.println("Invalid Kp command");
+        break;
+      }
+      Serial.println(Kp);
+      Serial.println(ACK);
+      break;
+    case SET_KD_CMD:
+      Serial.println(ACK);
+      while(!Serial.available());
+      Kd = ((int16_t) Serial.read())<<8;
+      while(!Serial.available());
+      Kd |= (int16_t) Serial.read();
+      while(!Serial.available());
+      if(Serial.read() != '\n')
+      {
+        Serial.println("Invalid Kd command");
+        break;
+      }
+      Serial.println(Kd);
+      Serial.println(ACK);
+      break;
+    case SET_KI_CMD:
+      Serial.println(ACK);
+      while(!Serial.available());
+      Ki = ((int16_t) Serial.read())<<8;
+      while(!Serial.available());
+      Ki |= (int16_t) Serial.read();
+      while(!Serial.available());
+      if(Serial.read() != '\n')
+      {
+        Serial.println("Invalid Ki command");
+        break;
+      }
+      Serial.println(Ki);
+      Serial.println(ACK);
+      break;
+    case SET_OFFSET_CMD:
+      Serial.println(ACK);
+      while(!Serial.available());
+      offset = ((int16_t) Serial.read()) << 8;
+      while(!Serial.available());
+      offset |= (int16_t) Serial.read();
+      while(!Serial.available());
+      if(Serial.read() != '\n')
+      {
+        Serial.print("Invalid offset command");
+        break;
+      }
+      Serial.println(offset);
+      Serial.println(ACK);
+      break;
+    case COMPUTE_LUT_CMD:
+      Serial.println(ACK);
+      for(int16_t i=0; i<SAMPLES_PER_PERIOD; ++i)
+      {
+        signal[i] = amplitude*sin(2*M_PI*((float)i/SAMPLES_PER_PERIOD))+base;
+        Serial.println(signal[i]);
+      }
+      Serial.println(ACK);
+      break;
+    case BEGIN_CMD:
+      Serial.println(ACK);
+      target = base;
+      setSpeed(offset);
+      delay(5000);
+      MsTimer2::start();
+      for(uint8_t i=0; i<periods_wait; ++i)
+      {
+        for(uint8_t j=0; j<SAMPLES_PER_PERIOD; ++j)
         {
-          N = Nm;
-          i = 0;
-          measurements_idx = 0;
-          state = measuring;
+          uint32_t t0 = micros();
+          target = signal[j];
+          if(micros()-t0 >= sample_period_us)
+          {
+            Serial.println(NACK);
+            break;
+          }
+          while(micros()-t0 < sample_period_us);
         }
       }
-      break;
-    case measuring:
-      measurements[measurements_idx] = analogRead(SENSOR_PIN);
-      measurements_idx += 1;
-      if(i < NUM_SAMPLES)
+      for(uint8_t i=0; i<periods_measure; ++i)
       {
-        ESC.writeMicroseconds(map(sin_lut(i), 0, 1000, 1000, 2000));
-        i += 1;
-      }
-      else
-      {
-        N -= 1;
-        if(N == 0)
+        for(uint8_t j=0; j<SAMPLES_PER_PERIOD; ++j)
         {
-          ESC.writeMicroseconds(map(0, 0, 1000, 1000, 2000));
-          state = idle;
-          timer.cancel();
-          for(uint16_t i=0; i<NUM_SAMPLES*Nm; ++i)
-            Serial.println(measurements[i]);
+          uint32_t t0 = micros();
+          target = signal[j];
+          datapoints[j+SAMPLES_PER_PERIOD*i] = analogRead(SENSOR_PIN);
+          if(micros()-t0 >= sample_period_us)
+          {
+            Serial.println(NACK);
+            break;
+          }
+          while(micros()-t0 < sample_period_us);
         }
       }
+      MsTimer2::stop();
+      setSpeed(0);
+      Serial.println(ACK);
+      for(uint16_t i=0; i<SAMPLES_PER_PERIOD*periods_measure; ++i)
+        Serial.println(datapoints[i]);
+      Serial.println(ACK);
+      break;
+    default:
+      Serial.print("Invalid command received: "); Serial.println(cmd);
       break;
   }
-  return true;
 }
